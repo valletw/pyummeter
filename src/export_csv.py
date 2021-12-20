b22cdfad63bca0e6f83a18e1b6c8c44a5670418a
William Vallet <valletw@gmail.com>
2021-12-20 12:26:59 +0100

    Provide CSV export method

---
+++ src/export_csv.py
@@ -0,0 +1,93 @@
+""" CSV export manager """
+import csv
+from datetime import datetime, timedelta
+from typing import Callable, Optional
+from src.ummeter import UMmeterData
+
+
+def bool_to_str(value: bool) -> str:
+    """ Convert bool to string """
+    return str(int(value))
+
+
+def datetime_to_str(value: datetime) -> str:
+    """ Convert date to string (ISO format) """
+    return value.isoformat(sep=" ")
+
+
+def timedelta_to_str(value: timedelta) -> str:
+    """ Convert time delta to string (elapsing seconds) """
+    return str(int(value.total_seconds()))
+
+
+class ExportCSV:
+    """ CSV export instance """
+    _SEP = ";"
+    _FIELD_DATE = ("", "Date", datetime_to_str)
+    _FIELDS = [
+        # UM-Meter field, Description, Conversion method.
+        ("voltage", "Voltage (V)", None),
+        ("intensity", "Intensity (A)", None),
+        ("power", "Power (W)", None),
+        ("resistance", "Resistance (Ohm)", None),
+        ("usb_voltage_dp", "USB D+ (V)", None),
+        ("usb_voltage_dn", "USB D- (V)", None),
+        ("charging_mode", "Charging Mode", None),
+        ("temperature_celsius", "Temperature (°C)", None),
+        ("model", "Model", None),
+        ("record_enabled", "Recording", bool_to_str),
+        ("record_duration", "Record duration (sec)", timedelta_to_str),
+        ("record_intensity_threshold", "Record intensity (A)", None),
+        ("record_capacity_threshold", "Record capacity (Ah)", None),
+        ("record_energy_threshold", "Record energy (Wh)", None),
+    ]
+    _FIELDS_DG = [
+        # UM-Meter field, Description.
+        ("capacity", "Capacity (Ah)", None),
+        ("energy", "Energy (Wh)", None),
+    ]
+
+    def __init__(self, filename: str):
+        assert filename is not None
+        assert len(filename) != 0
+        self._path = filename
+        # Prepare description row.
+        desc_row = [self._FIELD_DATE[1]]
+        desc_row.extend([d[1] for d in self._FIELDS])
+        desc_row.extend([d[1] for d in self._FIELDS_DG])
+        # Write description row, and create CSV file.
+        with open(self._path, "w", encoding="utf-8") as csv_f:
+            csv_w = csv.writer(
+                csv_f, delimiter=self._SEP, quoting=csv.QUOTE_MINIMAL)
+            csv_w.writerow(desc_row)
+
+    def __str__(self):
+        return f"<ExportCSV: path={self._path}>"
+
+    @staticmethod
+    def _convert(convert: Optional[Callable], value) -> str:
+        if convert is None:
+            return str(value)
+        return convert(value)
+
+    def update(self, date: datetime, data: UMmeterData):
+        """ Write data to export file """
+        # Prepare values to export.
+        val = [
+            self._convert(self._FIELD_DATE[2], date)
+        ]
+        val.extend([
+            self._convert(f[2], data[f[0]])  # type: ignore[misc]
+            for f in self._FIELDS
+        ])
+        val.extend([
+            self._convert(
+                f[2],
+                data["data_group"][data["data_group_selected"]][f[0]])  # type: ignore[misc]
+            for f in self._FIELDS_DG
+        ])
+        # Write value to CSV file.
+        with open(self._path, "a", encoding="utf-8") as csv_f:
+            csv_w = csv.writer(
+                csv_f, delimiter=self._SEP, quoting=csv.QUOTE_MINIMAL)
+            csv_w.writerow(val)
---
+++ tests/test_export_csv.py
@@ -0,0 +1,73 @@
+import pytest
+import unittest
+from datetime import datetime, timedelta
+from src.export_csv import ExportCSV
+from src.ummeter import UMmeterData
+
+
+@pytest.fixture
+def mfile(mocker):
+    return mocker.patch("builtins.open", unittest.mock.mock_open())
+
+
+class TestExportCSV:
+    def test_init(self, mfile):
+        with pytest.raises(AssertionError):
+            ExportCSV(None)
+        with pytest.raises(AssertionError):
+            ExportCSV("")
+        export = ExportCSV("test.csv")
+        mfile().write.assert_called_once()
+        mfile().write.assert_called_with(
+            "Date;Voltage (V);Intensity (A);Power (W);Resistance (Ohm);USB D+ (V);"
+            "USB D- (V);Charging Mode;Temperature (°C);Model;Recording;"
+            "Record duration (sec);Record intensity (A);Record capacity (Ah);"
+            "Record energy (Wh);Capacity (Ah);Energy (Wh)\r\n")
+        assert str(export) == "<ExportCSV: path=test.csv>"
+
+    def test_update(self, mfile):
+        date = datetime.now()
+        data: UMmeterData = {
+            "model": "UM34C",
+            "voltage": 5.10,
+            "intensity": 0.328,
+            "power": 1.672,
+            "resistance": 9999.9,
+            "usb_voltage_dp": 0.01,
+            "usb_voltage_dn": 0.02,
+            "charging_mode": "DCP1.5A",
+            "charging_mode_full": "Dedicated Charging Port (max. 1.5 A)",
+            "temperature_celsius": 20,
+            "temperature_fahrenheit": 68,
+            "data_group_selected": 0,
+            "data_group": [
+                {"capacity": 0.011, "energy": 0.056},
+                {"capacity": 0.0, "energy": 0.0},
+                {"capacity": 0.0, "energy": 0.0},
+                {"capacity": 0.0, "energy": 0.0},
+                {"capacity": 0.0, "energy": 0.0},
+                {"capacity": 0.0, "energy": 0.0},
+                {"capacity": 0.0, "energy": 0.0},
+                {"capacity": 0.0, "energy": 0.0},
+                {"capacity": 0.0, "energy": 0.0},
+                {"capacity": 0.0, "energy": 0.0}
+            ],
+            "record_capacity_threshold": 0.016,
+            "record_energy_threshold": 0.256,
+            "record_intensity_threshold": 0.1,
+            "record_duration": timedelta(seconds=240),
+            "record_enabled": False,
+            "screen_index": 2,
+            "screen_timeout": timedelta(minutes=2),
+            "screen_brightness": 4,
+            "checksum": 0x8c,
+        }
+        export = ExportCSV("test.csv")
+        mfile().reset_mock()
+        export.update(date, data)
+        mfile().write.assert_called_once()
+        mfile().write.assert_called_with(
+            f"{date.isoformat(sep=' ')};"
+            "5.1;0.328;1.672;9999.9;0.01;0.02;DCP1.5A;20;UM34C;0;240;0.1;"
+            "0.016;0.256;0.011;0.056\r\n"
+        )
