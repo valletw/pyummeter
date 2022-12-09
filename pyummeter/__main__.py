2c32b11fd4ed2f067a7154d146e8148093958ace
Rogier van Staveren <rogier@batoid.com>
2022-12-09 16:42:45 +0100

    Handle KeyboardInterrupt

--- pyummeter/__main__.py
+++ pyummeter/__main__.py
@@ -25,16 +25,23 @@ if __name__ == "__main__":
     export: Optional[ExportCSV] = None
     if params.export != "":
         export = ExportCSV(params.export)
-    # Run data dump process.
-    with UMmeter(params.tty) as meter:
-        while True:
-            data = meter.get_data()
-            now = datetime.now()
-            if export is not None:
-                export.update(now, data)
-            print(
-                f"[{data['model']}] {now.time()}"
-                f" {data['voltage']:1.04f}V {data['intensity']:1.04f}A"
-                f" {data['power']:1.04f}W {data['resistance']}Ohm"
-            )
-            sleep(params.refresh)
+
+    try:
+        # Run data dump process.
+        with UMmeter(params.tty) as meter:
+            while True:
+                data = meter.get_data()
+                now = datetime.now()
+                if export is not None:
+                    export.update(now, data)
+                print(
+                    f"[{data['model']}] {now.time()}"
+                    f" {data['voltage']:1.04f}V {data['intensity']:1.04f}A"
+                    f" {data['power']:1.04f}W {data['resistance']}Ohm"
+                )
+                sleep(params.refresh)
+    except KeyboardInterrupt:
+        # Handle keyboard interrupt
+        pass
+    finally:
+        meter.close()
